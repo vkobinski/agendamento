d62ee549bf4eb9dac798fbd82554779cbd8291a2
vkobinski <victorkobinski@outlook.com>
2024-03-24 16:25:19 -0300

    Secretaria

--- AgendamentoCliente/Models/Utils.cs
+++ AgendamentoCliente/Models/Utils.cs
@@ -2,8 +2,8 @@
 {
     internal static class Utils
     {
-        //private static string ip = "http://154.56.43.220:8080";
-        private static string ip = "http://192.168.2.101:8080";
+        private static string ip = "http://154.56.43.220:8080";
+        //private static string ip = "http://192.168.2.101:8080";
         public static string GetIp(string path)
         {
             return ip + path;
